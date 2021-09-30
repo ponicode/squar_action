d2aabfd40c73e90e59ba1d46e0e051cbedcf68e1
Baptiste Bouffaut <baptiste.bouffaut@gmail.com>
2021-09-30 23:44:44 +0200

    slightly improve the Hello-Worls index file ;-)

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log('Hello world!')
+function main(args: string[]): void {
+    const arg1: string = args[0];
+    const arg2: string = args[1];
+
+    console.log(arg1, arg2);
+}
+
+main(process.argv.slice(2))
