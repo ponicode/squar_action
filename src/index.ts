e352c19f0f85fc2125ae2a65eb2466f43d6c007d
Baptiste Bouffaut <baptiste.bouffaut@gmail.com>
2021-10-01 16:55:06 +0200

    Manage entrypoint input parameters

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,49 @@
-function main(args: string[]): void {
-    const arg1: string = args[0];
-    const arg2: string = args[1];
+import { Logger } from "tslog";
 
-    console.log(arg1, arg2);
+// Action Inputs Type definiction
+interface Inputs {
+    RepoUrl: string;
+    UserToken: string;
+    ImpactedFiles: string[];
+    Branch: string;
 }
 
-main(process.argv.slice(2))
+const log: Logger = new Logger();
+
+/** 
+* Checks all args are well defined
+* @param {string[]} args - arguments received from the command-line
+* @return {boolean} returns true if all arguments are well defined. False else.
+*/
+function check_args(args: string[]): boolean {
+    let result: boolean = true;
+    for (let i = 0; i < 4; i++ ) {
+        result = result && (args[i] !== undefined);
+    }
+    return result;
+}
+/** 
+* Main entry point.
+* @param {string[]} args - arguments received from the command-line
+* @return {void} 
+*/
+function main(args: string[]): void {
+    if (check_args(args)) {
+
+        const inputs = {
+            RepoUrl: args[0],
+            UserToken: args[1],
+            // tslint:disable-next-line: object-literal-sort-keys
+            ImpactedFiles: args[2] as unknown as string[],
+            Branch: args[3],
+        } as Inputs;
+
+        // tslint:disable-next-line: no-console
+        log.debug(inputs);
+    } else {
+        // tslint:disable-next-line: no-console
+        log.error("Missing Parameters");
+    }
+}
+
+main(process.argv.slice(2));
