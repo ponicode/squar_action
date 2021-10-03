67cab3fa2a4656d7be9e5c0f2f430dc6ea1dfdfb
Baptiste Bouffaut <baptiste.bouffaut@gmail.com>
2021-10-03 11:46:26 +0200

    transform the project into a node12 GitHib Action

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
+import * as core from "@actions/core";
+import * as github from "@actions/github";
 import * as dotenv from "dotenv";
 import { Logger } from "tslog";
+import {parseInputs} from "./inputs";
 import { triggerSquarEvaluate, triggerSquarReport } from "./squar_client";
 import { EvaluateReturn, FetchReportInput, Inputs, Report } from "./types";
 
@@ -25,34 +28,31 @@ function check_args(args: string[]): boolean {
 * @param {string[]} args - arguments received from the command-line
 * @return {void} 
 */
-function main(args: string[]): void {
+async function run(): Promise<void> {
 
-    const inputs: Inputs = JSON.parse(args[0]);
+    core.debug(`Parsing inputs`);
+    const inputs = parseInputs(core.getInput);
 
     log.debug(inputs);
 
     if (process.env.FETCH_REPORT_RETRY_MILLISEC !== undefined) {
         // Trigger SQUAR evaluate_pr endpoint
-        triggerSquarEvaluate(inputs).then((result: EvaluateReturn) => {
-            log.debug(result);
-            const reportInputs: FetchReportInput = {
-                userToken: inputs.userToken,
-            };
+        const result: EvaluateReturn = await triggerSquarEvaluate(inputs);
+        log.debug(result);
+        const reportInputs: FetchReportInput = {
+            userToken: inputs.userToken,
+        };
 
-            // If repository_id is defined then retry fetchReport until we get it
-            if ((result.repositoryId !== undefined) && (process.env.FETCH_REPORT_RETRY_MILLISEC !== undefined)) {
-                // tslint:disable-next-line: max-line-length
-                triggerSquarReport(reportInputs, result.repositoryId, parseInt(process.env.FETCH_REPORT_RETRY_MILLISEC, 10)).then((reportResult: Report) => {
-                    log.debug(reportResult);
-                });
-            } else {
-                log.error("No Repository Id");
-            }
-        }).catch((result: EvaluateReturn) => {
-            log.debug(result);
-        });
+        // If repository_id is defined then retry fetchReport until we get it
+        if ((result.repositoryId !== undefined) && (process.env.FETCH_REPORT_RETRY_MILLISEC !== undefined)) {
+            // tslint:disable-next-line: max-line-length
+            const reportResult: Report = await triggerSquarReport(reportInputs, result.repositoryId, parseInt(process.env.FETCH_REPORT_RETRY_MILLISEC, 10));
+            log.debug(reportResult);
+        } else {
+            log.error("No Repository Id");
+        }
     }
 
 }
 
-main(process.argv.slice(2));
+void run();
---
+++ src/inputs.ts
@@ -0,0 +1,35 @@
+import {InputOptions} from "@actions/core";
+import {Inputs} from "./types";
+
+type GetInput = (name: string, options?: InputOptions | undefined) => string;
+
+const parseJSON = <T>(getInput: GetInput, property: string): T | undefined => {
+  const value = getInput(property);
+  if (!value) {
+    return;
+  }
+  try {
+    return JSON.parse(value) as T;
+  } catch (e) {
+    const error = e as Error;
+    throw new Error(`invalid format for '${property}: ${error.toString()}`);
+  }
+};
+
+export const parseInputs = (getInput: GetInput): Inputs => {
+  const repoURL = getInput("repoURL", {required: true});
+  const userToken = getInput("userToken", {required: true});
+  let impactedFiles = parseJSON<string[]>(getInput, "impactedFiles");
+  const branch = getInput("branch", { required: true});
+
+  if (!impactedFiles) {
+    impactedFiles = [];
+  }
+
+  return {
+    repoURL,
+    userToken,
+    impactedFiles,
+    branch,
+  };
+};
