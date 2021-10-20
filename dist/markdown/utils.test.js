73400d7381140585233f541aa336a9c2d65b929f
Baptiste Bouffaut <bbouffaut@users.noreply.github.com>
2021-10-20 14:58:14 +0200

    index dist folder so that this is usable by Action release (#40)
    * index dist folder so that this is usable by Action release
    
    * Update README

---
+++ dist/markdown/utils.test.js
@@ -0,0 +1,88 @@
+"use strict";
+// // import * as utils from "./utils"
+// // import * as types from "../types"
+// // // @ponicode
+// // describe("utils.initMarkdownTable", () => {
+// //     test("0", () => {
+// //         let callFunction: any = () => {
+// //             utils.initMarkdownTable()
+// //         }
+// //     
+// //         expect(callFunction).not.toThrow()
+// //     })
+// // })
+// // 
+// // // @ponicode
+// // describe("utils.translateAlertType", () => {
+// //     test("0", () => {
+// //         let callFunction: any = () => {
+// //             utils.translateAlertType(undefined)
+// //         }
+// //     
+// //         expect(callFunction).not.toThrow()
+// //     })
+// // })
+// // 
+// // // @ponicode
+// // describe("utils.translateCriticity", () => {
+// //     test("0", () => {
+// //         let callFunction: any = () => {
+// //             utils.translateCriticity(undefined)
+// //         }
+// //     
+// //         expect(callFunction).not.toThrow()
+// //     })
+// // })
+// // 
+// // // @ponicode
+// // describe("utils.buildGithubFileURL", () => {
+// //     test("0", () => {
+// //         let callFunction: any = () => {
+// //             utils.buildGithubFileURL({ id: "a1969970175", repository_id: 987650, alert_kind: types.AlertKind."test_suite", line: -100, criticity: types.Criticity."orange", file_path: "/usr/sbin/tan_district.geo.qxt" }, "https://twitter.com/path?abc", "protocol-reboot")
+// //         }
+// //     
+// //         expect(callFunction).not.toThrow()
+// //     })
+// // 
+// //     test("1", () => {
+// //         let callFunction: any = () => {
+// //             utils.buildGithubFileURL({ id: "a1969970175", repository_id: 56784, alert_kind: types.AlertKind."edge_case", line: -100, criticity: types.Criticity."orange", file_path: "/net/panel.dvi.crd" }, "https://croplands.org/app/a/reset?token=", "protocol-reboot")
+// //         }
+// //     
+// //         expect(callFunction).not.toThrow()
+// //     })
+// // 
+// //     test("2", () => {
+// //         let callFunction: any = () => {
+// //             utils.buildGithubFileURL({ id: "bc23a9d531064583ace8f67dad60f6bb", repository_id: "a1969970175", alert_kind: types.AlertKind."edge_case", line: -100, criticity: types.Criticity."green", file_path: "/usr/sbin/tan_district.geo.qxt" }, "https://croplands.org/app/a/confirm?t=", "microchip-bypass")
+// //         }
+// //     
+// //         expect(callFunction).not.toThrow()
+// //     })
+// // 
+// //     test("3", () => {
+// //         let callFunction: any = () => {
+// //             utils.buildGithubFileURL({ id: "a1969970175", repository_id: "a1969970175", alert_kind: types.AlertKind."test_case", line: -5.48, criticity: types.Criticity."green", file_path: "/tmp/payment_invoice.ogg.cmc" }, "https://api.telegram.org/bot", "microchip-bypass")
+// //         }
+// //     
+// //         expect(callFunction).not.toThrow()
+// //     })
+// // 
+// //     test("4", () => {
+// //         let callFunction: any = () => {
+// //             utils.buildGithubFileURL({ id: "bc23a9d531064583ace8f67dad60f6bb", repository_id: "bc23a9d531064583ace8f67dad60f6bb", alert_kind: types.AlertKind."edge_case", line: 0, criticity: types.Criticity."orange", file_path: "/usr/sbin/tan_district.geo.qxt" }, "https://", "matrix-input")
+// //         }
+// //     
+// //         expect(callFunction).not.toThrow()
+// //     })
+// // 
+// //     test("5", () => {
+// //         let callFunction: any = () => {
+// //             utils.buildGithubFileURL({ id: Infinity, repository_id: Infinity, alert_kind: types.AlertKind."test_suite", line: Infinity, criticity: types.Criticity."orange", file_path: "" }, "", "")
+// //         }
+// //     
+// //         expect(callFunction).not.toThrow()
+// //     })
+// // })
+// // 
+// 
