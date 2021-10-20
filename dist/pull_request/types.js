844b525e777397ca9d97b3c684af68a350bf23b6
Baptiste Bouffaut <bbouffaut@users.noreply.github.com>
2021-10-20 16:00:39 +0200

    Bundle JS package for release

--- dist/pull_request/types.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.PONICODE_UT_BRANCH = void 0;
-var PONICODE_UT_BRANCH = "ponicode-ut-proposal";
-exports.PONICODE_UT_BRANCH = PONICODE_UT_BRANCH;
